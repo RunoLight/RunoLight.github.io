2d373cf5153e4a5db3f622e4133e31187a671d70
Runo <31756050+RunoLight@users.noreply.github.com>
2020-12-09 12:51:26 +1000

    s

--- js/postsHandler.js
+++ js/postsHandler.js
@@ -4,7 +4,7 @@ const addPostForm = document.querySelector('.add-post');
 const editPostForm = document.querySelector('.edit-post');
 const nextPageButton = document.querySelector('.next-page-button');
 const prevPageButton = document.querySelector('.prev-page-button');
-const adminEmail = 'vblimov@gmail.com'
+const adminEmail = 'vasia-cotic@yandex.ru'
 let editablePostID;
 let showedPosts = 0;
 let postsCount = 999999;
@@ -235,77 +235,3 @@ const editPost = (postID) => {
 document.addEventListener('DOMContentLoaded', ()=> {
     postHandlerInit();
 })
-
-
-/*database.ref('post').orderByChild('id').once('value', snapshot => {
-        (snapshot || []).forEach(item=>{
-            
-            posts.push(`
-              <section class="post">
-                <div class="post-body">
-                  <h2 class="post-title">${item.val().title}</h2>
-                  <p class="post-text" >${item.val().text}</p>
-                  <div class="tags">
-                    ${(item.val().tags || []).map(tag=>`<a href="#" class="tag">#${tag}</a>`)}
-                  </div>
-                </div>
-                <div class="post-footer">
-                  <div class="post-buttons">
-                    <button class="post-button post-button-likes">
-                <svg width="19" height="20" class="icon icon-like">
-                  <use xlink:href="img/icons.svg#like"></use>
-                </svg>
-                <span class="post-button likes-counter">${item.val().like}</span>
-              </button>
-                    <button class="post-button post-button-comments">
-                <svg width="21" height="21" class="icon icon-comment">
-                  <use xlink:href="img/icons.svg#comment"></use>
-                </svg>
-              <span class="comments-counter">${item.val().comments}</span>
-            </button>
-                    <button class="post-button post-button-save">
-                <svg width="19" height="19" class="icon icon-save">
-                  <use xlink:href="img/icons.svg#save"></use>
-                </svg>
-            </button>
-                    <button class="post-button post-button-share">
-                <svg width="17" height="19" class="icon icon-share">
-                  <use xlink:href="img/icons.svg#share"></use>
-                </svg>
-            </button>
-                    ${setUsers.user? item.val().author.email === (setUsers.user.email || null) || setUsers.user.email === adminEmail?
-                          `<button class="post-button post-button-delete" value="${item.val().id}" onclick="deletePost(value)">
-                  <svg width="17" height="19" class="icon icon-delete">
-                    <use xlink:href="img/icons.svg#delete"></use>
-                  </svg>
-                </button>`: `` :``}
-                    ${setUsers.user? item.val().author.email === (setUsers.user.email || null) || setUsers.user.email === adminEmail?
-                `<button class="post-button post-button-edit" value="${item.val().id}" onclick="editPost(value)">
-                  <svg width="17" height="19" class="icon icon-edit">
-                    <use xlink:href="img/icons.svg#edit"></use>
-                  </svg>
-                </button>`: `` :``}
-                  </div>
-                  <div class="post-author">
-                    <div class="author-about">
-                      <a href="#" class="author-username">${item.val().author.displayName}</a>
-                      <span class="post-time">${item.val().date}</span>
-                    </div>
-                  <a href="#" class="author-link"><img src=${item.val().author.photo || "img/avatar.jpeg"} alt="avatar" class="author-avatar"></a>
-                </div>
-                </div>
-              </section>`);
-            counter++;
-        })
-    }).then(()=> {
-        posts.reverse().forEach(post => {postsHTML+=post;})
-        postsHTML+=`<div class="post-buttons">
-                        <button class="page-buttons prev-page-button">\<</button>
-                        <button class="page-buttons next-page-button">\></button>
-                  </div>`;
-        postsWrapper.innerHTML = postsHTML;
-        postsHTML = '';
-        addPostForm.classList.remove('visible');
-        editPostForm.classList.remove('visible');
-        postsWrapper.classList.add('visible');
-    })*/
